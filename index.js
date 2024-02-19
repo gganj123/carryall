c25e1787d0a01b1b0f23289fd0960cea2b6e156a
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-02-20 01:48:51 +0900

    test request

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 
-
 app.get('/', (req, res) => {
-  res.send('OK');
+    res.send('GET /users');
 });
+
+app.post('/', (req, res) => {
+    res.send('POST /users');
+});
+
+app.put('/', (req, res) => {
+    res.send('PUT /users');
+});
+
+app.delete('/', (req, res) => {
+    res.send('DELETE /users');
+});
+
 
 app.listen(process.env.PORT);
