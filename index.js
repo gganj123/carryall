43077feb593000056200c84acedc66b7dcdd90dd
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-02-21 23:55:30 +0900

    feat: elice first project main page

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+
+app.get('/', (req, res) => {
+    res.send('GET /users');
+});
+
+app.post('/', (req, res) => {
+    res.send('POST /users');
+});
+
+app.put('/', (req, res) => {
+    res.send('PUT /users');
+});
+
+app.delete('/', (req, res) => {
+    res.send('DELETE /users');
+});
+
+
+app.listen(process.env.PORT);
