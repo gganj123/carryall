b238f9036e09929ccf32cba2e2ff987e682dc8a5
jjajan2 <hje0514@gmail.com>
2024-02-22 14:49:27 +0900

    docs: cart.js 수정

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,33 +1,11 @@
 const express = require('express');
 const cartRouter = express.Router();
 
-const User = mongoose.model('user', new mongoose.Schema({
-    name: String,
-    cart: [
-        {
-            productId : Number,
-            quantity : Number,
-        }
-    ],
-}));
 
 
-// POST /cart/:productId
+
 cartRouter.post('/cart', async(req, res) => {
     const { cart } = req.body;
 
     console.log(cart)
 })
-
-
-// router.post('/', async(req, res, next) => {
-//     try {
-//         const { email, password, isAdmin } = req.body;
-
-//         const savedUser = await User.create({ email, password, isAdmin });
-
-//         res.status(201).json(utils.buildResponse(savedUser));
-//     } catch (error) {
-//         next(error);
-//     }
-// })
