e398ff9cd8a1377b60d4fb8bc34f437d8c824f21
[SW8] 박유신 <yushin.pak@gmail.com>
2024-02-23 04:10:35 +0000

    Feat: order 모델 추가

--- server/models/index.js
+++ server/models/index.js
@@ -5,13 +5,17 @@ const CategorySchema = require("./schemas/category");
 const CartSchema = require("./schemas/cart");
 const UserSchema = require("./schemas/user");
 const AddressSchema = require("./schemas/address");
+const OrderSchema = require("./schemas/order");
+
+
 const Product = model("Product", ProductSchema);
 const Category = model("Category", CategorySchema);
 const Cart = model("Cart", CartSchema);
 const User = model("User", UserSchema);
 const Address = model("Address", AddressSchema);
+const Order = model("Order", OrderSchema);
 
 
-module.exports = { Product, Category, Cart, User, Address };
+module.exports = { Product, Category, Cart, User, Address, Order };
 
 
