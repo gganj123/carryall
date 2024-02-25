4340e87787c04453c220ec2f26ed2fb03b94b11e
gganj123 <zwol10@naver.com>
2024-02-25 16:44:04 +0900

    feat: objectList, dateSort

--- client/js/objectList.js
+++ client/js/objectList.js
@@ -6,16 +6,43 @@ axios.get('http://localhost:5000/product')
     console.log(res.data);
 
     productList.forEach(product => {
-      htmlString += `<div style="height: 200px; width: 23%; margin-right: 20px;">
+      htmlString += `<div style="height: 300px; width: 23%; margin-right: 20px;">
       <ul>`
-      htmlString += `<li><strong>Image:</strong> <img src="${product.image}" alt="Product Image"></li>`;//이미지 넣어서 나오게 해보기
-      htmlString += `<li><strong>Name:</strong> ${product.name}</li> `;
+      htmlString += `<li><img class="obimg" src="${product.image}" alt="Product Image"></li>`;//이미지 넣어서 나오게 해보기
+      htmlString += `<li><strong>Name: ${product.name}</strong></li> `;
       htmlString += `<li><strong>Brand:</strong> ${product.brand}</li>`;
-      htmlString += `<li><strong>Price:</strong> ${product.price}</li>`;
+      htmlString += `<li><strong>Price: ${product.price.toLocaleString('ko-KR')}</strong></li>`;
       htmlString += `</div>`;
     });
 
     document.getElementById('objectList').innerHTML = htmlString;
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+
+  axios.get('http://localhost:5000/product')
+  .then(res => {
+    // 등록일순으로 데이터 정렬
+    const productList = res.data.sort((a, b) => {
+      return new Date(a.createdAt) - new Date(b.createdAt);
+    });
+
+    let htmlString = '';
+    console.log(productList); // 정렬된 productList를 출력
+
+    productList.forEach(product => {
+      htmlString += `<div class ="font_17" style="height: 300px; width: 23%; margin-right: 20px;">
+      <ul>`
+      htmlString += `<li><img class="obimg" src="${product.image}" alt="${product.name}"></li>`;// 이미지 삽입 부분
+      htmlString += `<li><strong>Name:${product.name}</strong> </li> `;
+      htmlString += `<li><strong>Brand:</strong> ${product.brand}</li>`;
+      htmlString += `<li><strong>Price: ${product.price.toLocaleString('ko-KR')}</strong></li>`;
+      htmlString += `</div>`;
+    });
+
+    document.getElementById('obListDateSorting').innerHTML = htmlString;
   })
   .catch(error => {
     console.error('Error fetching data:', error);
