68ebd5f197a0ba8dfa9fa8afcd97f423acbecaf4
Joo Hyeon Jung <twincornjr@gmail.com>
2024-02-26 16:42:14 +0900

    fix: 회원가입 오류

--- server/utils/hashPassword.js
+++ server/utils/hashPassword.js
@@ -1,7 +1,11 @@
-const { createHash } = require('crypto') // 비밀번호 해시 때 사용하려고 미리 만들기~
-
-module.exports = (password) => {
-  const hash = createHash('bcrpyt');
-  hash.update(password);
-  return hash.digest("hex");
-}
+// const { createHash } = require('crypto') // 비밀번호 해시 때 사용하려고 미리 만들기~
+const bcrypt = require('bcrypt');
+module.exports = async (password) => {
+  const hash = await bcrypt.hash(password, 10);
+  return hash;
+};
+// module.exports = (password) => {
+//   const hash = createHash('bcrypt');
+//   hash.update(password);
+//   return hash.digest("hex");
+// }
