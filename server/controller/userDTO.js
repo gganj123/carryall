f48e8de6329752f6c09e26b2c17106cce8882ac4
jjajan2 <hje0514@gmail.com>
2024-02-26 22:23:49 +0900

    feat:userDTO.js 생성

---
+++ server/controller/userDTO.js
@@ -0,0 +1,7 @@
+class UserDTO {
+    constructor(username, name, email) {
+      this.username = username;
+      this.name = name;
+      this.email = email;
+    }
+  }
--- server/models/schemas/userNotification.js
+++ server/models/schemas/userNotification.js
@@ -3,6 +3,10 @@ const { Schema } = require("mongoose");
 //여러가지 체크를 할 수 있기 때문에 UserNotification 등의 테이블로 분리
 const UserNotificationSchema = new Schema(
   {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
     // 휴대폰 수신 동의
     telSubscription: {
       type: Boolean,
