2b45794571088e207cbec543ca14148c36ea4e39
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-02-28 23:46:47 +0900

    main-product sort func

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const express = require('express');
+const express = require("express");
+const fs = require('fs');
+const path = require('path');
+const cors = require("cors");
+const { json, urlencoded } = require("express");
 const app = express();
-require('dotenv').config();
+require("dotenv").config();
+const { PORT, MONGODB_PASSWORD } = process.env;
+const { connect } = require("mongoose");
+
+connect(
+  `mongodb+srv://carryall:${MONGODB_PASSWORD}@cluster0.lobzfqe.mongodb.net/`
+)
+  .then(() => console.log("connected"))
+  .catch(() => console.log("failed"));
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+
+const indexRouter = require('./server/routes');
+const productsRouter = require("./server/routes/products.js");
+const categoriesRouter = require("./server/routes/categories.js");
+const cartsRouter = require("./server/routes/carts.js");
+const ordersRouter = require("./server/routes/orders.js");
+const usersRouter = require("./server/routes/users.js");
+const viewRouter = require("./server/routes/viewRouter.js");
 
 
-app.get('/', (req, res) => {
-    res.send('GET /users');
+app.use(express.static('client'));
+app.use(viewRouter);
+
+app.use('/api', indexRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/categories", categoriesRouter);
+app.use("/api/carts", cartsRouter);
+app.use("/api/orders", ordersRouter);
+app.use("/api", usersRouter);
+
+app.get("/",(req, res)=> {
+  res.send("접속 성공");
 });
 
-app.post('/', (req, res) => {
-    res.send('POST /users');
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
-
-app.put('/', (req, res) => {
-    res.send('PUT /users');
-});
-
-app.delete('/', (req, res) => {
-    res.send('DELETE /users');
-});
-
-
-app.listen(process.env.PORT);
