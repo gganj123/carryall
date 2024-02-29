3bf5e98bf9835f482932d0387474990c165aaf24
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-02-29 17:12:07 +0900

    feat: category good

--- client/js/movepage.js
+++ client/js/movepage.js
@@ -1,6 +1,10 @@
   const stussyButton = document.getElementById('stussy');
   const supremeButton = document.getElementById('supreme');
   const bareButton = document.getElementById('bare');
+  const todButton = document.getElementById('tod');
+  const corssButton = document.getElementById('cross');
+  const backButton = document.getElementById('back');
+
 
   // Stussy 버튼 클릭 시 product 페이지로 이동
   stussyButton.addEventListener('click', function() {
@@ -15,4 +19,18 @@
   // Bare 버튼 클릭 시 product 페이지로 이동
   bareButton.addEventListener('click', function() {
     window.location.href = '/product?brand=bare';
+  });
+
+  todButton.addEventListener('click', function() {
+    window.location.href = '/product?categoryId=65df755367457938cc02e40b';
+  });
+
+  // Supreme 버튼 클릭 시 product 페이지로 이동
+  corssButton.addEventListener('click', function() {
+    window.location.href = '/product?categoryId=65df752e67457938cc02e405';
+  });
+
+  // Bare 버튼 클릭 시 product 페이지로 이동
+  backButton.addEventListener('click', function() {
+    window.location.href = '/product?categoryId=65df753a67457938cc02e408';
   });
