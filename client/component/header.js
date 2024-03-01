08379766465bed09da6c30d4bea6e32e9ff9aaaa
gganj123 <48715999+gganj123@users.noreply.github.com>
2024-03-01 23:29:07 +0900

    feat:first project

---
+++ client/component/header.js
@@ -0,0 +1,56 @@
+function loggedIn() {
+    axios
+      .get("/api/get-session")
+      .then((response) => {
+        const userData = response.data;
+  
+        if (userData.user.username === "adminCarryAll") {
+          console.log("관리자");
+        } else {
+          console.log("일반회원임");
+        }
+      })
+      .catch((error) => {
+        console.log("로그아웃됨");
+      });
+  }
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    // axios 라이브러리 추가
+    var script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
+    document.head.appendChild(script);
+  
+    var headerElement = document.getElementById("header");
+    headerElement.innerHTML = `
+      <div class="header"></div>
+      <div class="header" style="background-color: #fafafa; display: flex; justify-content: center; align-items: center;">
+          <button class="logoButton">
+              <h1 class="logo slideDown" style="color:black"><a href="/">CARRYALL</a></h1>
+          </button>
+          <input class="headInput" type="text" placeholder="검색">
+          <button class="headmenu"><a href="/cart">장바구니</a></button>
+          <button class="headmenu login"><a href="/loginMember">로그인</a></button>
+          <button class="headmenu logout"><a href="/">로그아웃</a></button>
+          <button class="headmenu mypage"><a href="/mypage">마이페이지</a></button>
+          <button class="headmenu admin" style="display: none;"><a href="/admin">관리자 페이지</a></button>
+          <button class="headmenu"><a href="/join">회원가입</a></button>
+      </div>`;
+  
+    loggedIn();
+  
+    document
+      .querySelector(".headmenu.logout")
+      .addEventListener("click", function (event) {
+        event.preventDefault();
+        axios
+          .post("/api/logout")
+          .then((response) => {
+            console.log("로그아웃 성공:", response.data);
+            window.location.href = "/";
+          })
+          .catch((error) => {
+            console.error("로그아웃 중 오류 발생:", error);
+          });
+      });
+  });
