c532d47839e1b1bdc08630b2a75b78778172c425
jjajan2 <hje0514@gmail.com>
2024-03-02 04:10:17 +0900

    fix:로그인 유효성검사 수정

--- server/routes/viewRouter.js
+++ server/routes/viewRouter.js
@@ -6,14 +6,12 @@ const adminVerification = require("../middlewares/adminVerification");
 viewRouter.use('/',serveStatic('main'));
 viewRouter.use('/product',serveStatic('product'));
 viewRouter.use('/admin', adminVerification, serveStatic('admin'));
-viewRouter.use('/adminTest', adminVerification, serveStatic('adminTest'));
-// 지은 {
+viewRouter.use('/admin/Product', adminVerification, serveStatic('adminProduct'));
 viewRouter.use('/admin/order', serveStatic('adminOrder'));
-// }
 viewRouter.use('/cart',serveStatic('cart'));
 viewRouter.use('/cartTest',serveStatic('cartTest'));
 viewRouter.use('/detail',serveStatic('detail'))
-viewRouter.use('/joinagree',serveStatic('joinagree'));
+viewRouter.use('/joinAgree',serveStatic('joinAgree'));
 viewRouter.use('/joinfinished',serveStatic('joinfinished'));
 viewRouter.use('/joinform',serveStatic('joinform'));
 viewRouter.use('/join',serveStatic('join'));
@@ -22,10 +20,7 @@ viewRouter.use('/loginMember',serveStatic('loginMember'));
 viewRouter.use('/loginnonMember',serveStatic('loginnonMember'));
 viewRouter.use('/order',serveStatic('order'));
 viewRouter.use('/practice',serveStatic('practice'));
-viewRouter.use('/test',serveStatic('test'));
 viewRouter.use('/mypage',serveStatic('mypage'));
-viewRouter.use('/admin/order',serveStatic('adminOrder'));
-viewRouter.use('/mypageTest',serveStatic('mypageTest'))
 viewRouter.use('/orderResult',serveStatic('orderResult'))
 viewRouter.use('/loginWithdraw',serveStatic('loginWithdraw'));
 viewRouter.use('/loginChange',serveStatic('loginChange'));
