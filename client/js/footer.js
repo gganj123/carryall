08fb9b31518488e16dded091ab484da3a7617c35
EunJi Kim <dust9629@gmail.com>
2024-03-02 10:14:09 +0900

    Feat : modify css

--- client/js/footer.js
+++ client/js/footer.js
@@ -1,36 +1,38 @@
 document.addEventListener("DOMContentLoaded", function() {
     var footerElement = document.getElementById("footer");
     footerElement.innerHTML = `
-        <div class="content_wrap" style="width: 100%; height: 100%;">
+        <div class="contBox" style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; padding: 30px 0">
+          <div class="content_wrap" style="width: 95%; height: 100%;">
             <div class="content"><h1 class="logo" style="color:white; margin-top: 10px; margin-left: 30px;">CARRYALL</h1></div>
-            <div class="content" style="color:white; margin-top: 10px;">
-                <button class="footmenu">이용약관</button>
-                <button class="footmenu">개인정보처리방침</button>
-                <button class="footmenu">이용안내</button>
-                <button class="footmenu">입점 / 제휴안내</button>
+            <div class="content" style="color:white; margin-top: 10px; display: flex; justify-content: flex-end;">
+              <button class="footmenu">이용약관</button>
+              <button class="footmenu">개인정보처리방침</button>
+              <button class="footmenu">이용안내</button>
+              <button class="footmenu">입점 / 제휴안내</button>
             </div>
             <div class="content" style="color:white; font-weight:normal; margin-left: 20px;">
-                대표자 : 6팀 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 서울 성동구 아차산로 17길 48 성수낙낙 2층 엘리스랩 성수점<br>
-                사업자등록번호 : 123-45-67890 &emsp;&emsp; 통신판매업신고번호 : 제2017-대전유성-0009호 <br>  
-                개인정보처리담당자 : 엘리스 &nbsp;&emsp;&emsp;&emsp;  대표번호 : 070-4633-2017       이메일 : kdp@elice.io
-                <div style="color:white; position: absolute; bottom: 0;">
-                    저작권.2024 엘리스랩. 판권 소유.
-                </div>
+              대표자 : 6팀 &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp; 서울 성동구 아차산로 17길 48 성수낙낙 2층 엘리스랩 성수점<br>
+              사업자등록번호 : 123-45-67890 &emsp;&emsp; 통신판매업신고번호 : 제2017-대전유성-0009호 <br>  
+              개인정보처리담당자 : 엘리스 &nbsp;&emsp;&emsp;&emsp;  대표번호 : 070-4633-2017       이메일 : kdp@elice.io
+              <div style="color:white; position: absolute; bottom: 30px; font-size: 15px;">
+                저작권.2024 엘리스랩. 판권 소유.
+              </div>
             </div>
             <div class="content" style="color:white;">
-                <div style="display: flex; flex-wrap: wrap;">
-                    <div style="width:30%; color: white;">
-                        엘리스랩 고객만족센터<br>
-                        070-4633-2017<br>
-                        평일 오전 9시~5시  /  토, 일, 공휴일 휴무
-                    </div>
-                    <div style="width:50%; color: white;">
-                        입금계좌안내<br>
-                        000-012345-12-123<br>
-                        IBK 기업은행 예금주 : 엘리스랩 성수점
-                    </div>
+              <div style="display: flex; flex-wrap: wrap; justify-content: flex-end; margin-left: 25px;">
+                <div style="width:30%; color: white;">
+                  엘리스랩 고객만족센터<br>
+                  070-4633-2017<br>
+                  평일 오전 9시~5시  /  토, 일, 공휴일 휴무
                 </div>
+                <div style="width:50%; color: white;">
+                  입금계좌안내<br>
+                  000-012345-12-123<br>
+                  IBK 기업은행 예금주 : 엘리스랩 성수점
+                </div>
+              </div>
             </div>
+          </div>
         </div>
     `;
 });
--- client/js/oblistsort.js
+++ client/js/oblistsort.js
@@ -1,46 +1,19 @@
-function fetchProducts() {
-  axios.get(`/api/products`)
-    .then(res => {
-      const productList = res.data;
-      let htmlString = '';
-      function getBrandFromURL() {
-        const urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get('brand');
-      }
-      const filteredProducts = productList.filter(product => product.brand.toLowerCase() === getBrandFromURL().toLowerCase()); // 대소문자 구분 없이 필터링
-        console.log(filteredProducts);
-        console.log(getBrandFromURL().toLowerCase());
-
-        filteredProducts.forEach(product => {
-        htmlString += `<div class="item" style="height: 321px; width: 244.44px; margin-bottom:100px;"><a href="/practice?id=${product._id}">`
-          htmlString += `<img class="img" style="width: 100%; height: auto;  display: block;" id="${product._id}" src="${product.image}">`;
-          htmlString += `<h2 id="root"> ${product.brand}</h2>`;
-          htmlString += `<div p>${product.name}</div> `;
-          htmlString += `<p>${product.price.toLocaleString('ko-KR')}</p>`;
-          htmlString += `</div></a>`;
-      });
-  
-      document.getElementById('oblistSort').innerHTML = htmlString;
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-}
-
-
-  function fetchProductsByBrand(brand) {
-    axios.get(`/api/products`)
+const urlParams = new URLSearchParams(window.location.search); //http://localhost:5001/product/?brand=bare
+const brand = urlParams.get('brand');
+changeText();
+ const bran =  brand.toUpperCase()
+ function fetchProductsByBrand(bran) {
+  console.log(bran);
+    axios.get(`/api/products?categoryName=${bran}`)
       .then(res => {
         const productList = res.data;
-
-        const filteredProducts = productList.filter(product => product.brand.toLowerCase() === brand.toLowerCase()); // 대소문자 구분 없이 필터링
-        console.log(filteredProducts);
         let htmlString = '';
- 
-        filteredProducts.forEach(product => {
+        console.log(res.data);
+        
+        productList.forEach(product => {
           htmlString += `<div class="item" style="height: 321px; width: 244.44px; margin-bottom:100px;"><a href="/practice?id=${product._id}">`
             htmlString += `<img class="img" style="width: 100%; height: auto;  display: block;" id="${product._id}" src="${product.image}">`;
-            htmlString += `<h2 id="root"> ${product.brand}</h2>`;
+            htmlString += `<h2 id="root"> ${product.categoryName}</h2>`;
             htmlString += `<div p>${product.name}</div> `;
             htmlString += `<p>${product.price.toLocaleString('ko-KR')}</p>`;
             htmlString += `</div></a>`;
@@ -53,61 +26,46 @@ function fetchProducts() {
       });
   }
 
+
+    function changeText() {
+      const urlParams2 = new URLSearchParams(window.location.search); //http://localhost:5001/product/?brand=bare
+      const brand2 = urlParams2.get('brand');
+        // chanText ID를 가진 요소 가져오기
+        const chanText = document.getElementById('chanText');
+        
+        // h2와 h3 요소 가져오기
+        const h2Element = chanText.querySelector('h2');
+
+        // 텍스트 변경
+        h2Element.textContent = `${brand2}`;
+    }
+
   const bareButton = document.getElementById('barebutt');
   const stussyButton = document.getElementById('stussybutt');
   const supremeButton = document.getElementById('supremebutt');
 
-
-bareButton.addEventListener('click', function() {
-    fetchProductsByBrand('bare');
+  bareButton.addEventListener('click', function() {
+    fetchProductsByBrand('BARE');
+    window.history.pushState(null, null, '?brand=bare');
+    changeText();
 });
 stussyButton.addEventListener('click', function() {
-    fetchProductsByBrand('stussy');
+  fetchProductsByBrand('STUSSY');
+  window.history.pushState(null, null, '?brand=stussy');
+  console.log("gg");
+  changeText();
 });
 supremeButton.addEventListener('click', function() {
-    fetchProductsByBrand('supreme');
+    fetchProductsByBrand('SUPREME');
+    window.history.pushState(null, null, '?brand=supreme');
+    changeText();
 });
-
-htmlString = '';
-function fetchProductsByCate(categoryId) {
-  axios.get(`/api/products`)
-    .then(res => {
-      const productList = res.data;
-
-      const filteredProduct = productList.filter(product => product.categoryId === categoryId);
-      console.log(productList);
-      let htmlString = '';
-  
-      filteredProduct.forEach(product => {
-        htmlString += `<div class="item" style="height: 321px; width: 244.44px; margin-bottom:100px;"><a href="/practice?id=${product._id}">`
-          htmlString += `<img class="img" style="width: 100%; height: auto;  display: block;" id="${product._id}" src="${product.image}">`;
-          htmlString += `<h2 id="root"> ${product.brand}</h2>`;
-          htmlString += `<div p>${product.name}</div> `;
-          htmlString += `<p>${product.price.toLocaleString('ko-KR')}</p>`;
-          htmlString += `</div></a>`;
-      });
-  
-      document.getElementById('oblistSort').innerHTML = htmlString;
-    })
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-}
-
-const tButton = document.getElementById('tbutt');
-const cButton = document.getElementById('cbutt');
-const bButton = document.getElementById('bbutt');
+// supremeButton.addEventListener('click', function() {
+//   fetchProductsByBrand('brandUp');
+//   window.history.pushState(null, null, `?brand=${brandUp}`);
+// });
 
 
-tButton.addEventListener('click', function() {
-  fetchProductsByCate('토트백');
-});
-cButton.addEventListener('click', function() {
-  fetchProductsByCate('크로스백');
-});
-bButton.addEventListener('click', function() {
-  fetchProductsByCate('백팩');
-});
 window.onload = function() {
-  fetchProducts();
+  fetchProductsByBrand(bran);
 };
